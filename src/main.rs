1e8216a7652243ffff1ee7e523b60688f65658fc
liam <liam.wirth1@outlook.com>
2024-02-05 19:09:17 -0700

    slkjfsd

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,12 @@ fn main() -> eframe::Result<()> {
 fn main() {
     // Redirect `log` message to `console.log` and friends:
     eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
+    let the_canvas_id = String::from("Cellular-Automata beta 0.0.1");
     let web_options = eframe::WebOptions::default();
-    web_options
     wasm_bindgen_futures::spawn_local(async {
         eframe::WebRunner::new()
             .start(
-                "balls", // hardcode it
+                "the_canvas_id", // hardcode it
                 web_options,
                 Box::new(|cc| Box::new(eframe_template::app::App::new(cc))),
             )
